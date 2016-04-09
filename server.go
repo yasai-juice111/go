284c9e9387bb2c1b974584aa5eff44bf34be61a6
cho_takumi <cho_takumi@grenge.co.jp>
2016-04-09 17:36:31 +0900

    Fisrt commit

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+  "net/http"
+  "text/template"
+)
+
+type Page struct {
+  Title string
+  Count int
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+  page := Page{"Hello World.", 1}
+  tmpl, err := template.ParseFiles("layout.html") // ParseFilesを使う
+  if err != nil {
+    panic(err)
+  }
+
+  err = tmpl.Execute(w, page)
+  if err != nil {
+    panic(err)
+  }
+}
+
+func main() {
+  http.HandleFunc("/", viewHandler) // hello
+  http.ListenAndServe(":3001", nil)
+}
---
+++ src/hello/app/controllers/app.go
@@ -0,0 +1,16 @@
+package controllers
+
+import "github.com/revel/revel"
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+
+func (c App) Hello() revel.Result {
+	return c.Render()
+}
